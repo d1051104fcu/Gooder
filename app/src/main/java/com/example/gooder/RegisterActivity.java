e999ebd00f1aa43c51fad8dbaabe4319b29cc129
User <d1051104fcu@gmail.com>
2025-05-19 16:06:20 +0800

    add LoginActivity 、 RegisterActivity. Need to fix: Nav to Home Screen. add User class.

---
+++ app/src/main/java/com/example/gooder/RegisterActivity.java
@@ -0,0 +1,119 @@
+package com.example.gooder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gooder.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText edt_Email, edt_Name, edt_Password, edt_ConfirmPassword;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        edt_Email = findViewById(R.id.register_edt_email);
+        edt_Name = findViewById(R.id.register_edt_name);
+        edt_Password = findViewById(R.id.register_edt_password);
+        edt_ConfirmPassword = findViewById(R.id.register_edt_ConfirmPassword);
+        Button register = findViewById(R.id.register_btn_register);
+        Button toLogin = findViewById(R.id.register_btn_toLogin);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edt_Email.getText().toString();
+                String name = edt_Name.getText().toString();
+                String password = edt_Password.getText().toString();
+                String confirmPassword = edt_ConfirmPassword.getText().toString();
+
+                if (!email.isEmpty() && !name.isEmpty() && !password.isEmpty()){
+                    if (password.equals(confirmPassword)){
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()){
+                                            FirebaseUser user = mAuth.getCurrentUser();
+
+                                            if (user != null){
+                                                String uid = user.getUid();
+                                                User userData = new User(uid, email, name);
+                                                db.collection("Users")
+                                                        .document(uid)
+                                                        .set(userData)
+                                                        .addOnSuccessListener( a -> {
+                                                            Log.i("User", "註冊成功！ " + uid);
+                                                        })
+                                                        .addOnFailureListener( e -> {
+                                                            Toast.makeText(RegisterActivity.this, "FireStore 寫入失敗：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                            Log.e("User", "註冊失敗！ " + e.getMessage());
+                                                        });
+                                            }
+
+                                            /*
+                                            // Fix: Nav to Home screen
+
+                                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                            finish();
+                                             */
+                                        }else {
+                                            Toast.makeText(RegisterActivity.this, "註冊失敗" + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
+                    }else {
+                        Toast.makeText(RegisterActivity.this, "確認密碼與密碼不同，請重新輸入", Toast.LENGTH_SHORT).show();
+                        edt_ConfirmPassword.setText("");
+                    }
+                }else {
+                    Toast.makeText(RegisterActivity.this, "輸入欄位不得為空", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        toLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
